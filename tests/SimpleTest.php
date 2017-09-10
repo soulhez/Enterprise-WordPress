8222886af3dd5d3715f1eb744117cd3238591a5a
Anthony Budd <anthonybudd94@gmail.com>
2017-09-10 19:03:15 +0100

    removed after success

---
+++ tests/SimpleTest.php
@@ -0,0 +1,13 @@
+<?php
+
+namespace Project;
+
+use PHPUnit\Framework\TestCase;
+
+class SimpleTest extends TestCase
+{
+    public function testBasicTest()
+    {
+        $this->assertTrue(true);
+    }
+}
